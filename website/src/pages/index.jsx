a873fa3714a553f42f87840f6c95a2f5858f9856
Mani Batra <manibatra2002@gmail.com>
2023-01-07 15:11:20 +1100

    remove unused imports

--- website/src/pages/index.jsx
+++ website/src/pages/index.jsx
@@ -1,16 +1,6 @@
 import Head from 'next/head'
-
-import { CallToAction } from '@/components/CallToAction'
-import { Faqs } from '@/components/Faqs'
-import { Footer } from '@/components/Footer'
-import { Header } from '@/components/Header'
-import { Hero } from '@/components/Hero'
-import { Pricing } from '@/components/Pricing'
-import { PrimaryFeatures } from '@/components/PrimaryFeatures'
-import { Reviews } from '@/components/Reviews'
-import { SecondaryFeatures } from '@/components/SecondaryFeatures'
-import ModelCarousel from '@/components/ModelCarousel'
-import EmblaCarousel from '@/components/carousel/EmblaCarousel'
+import {Header} from '@/components/Header'
+import {Hero} from '@/components/Hero'
 
 export default function Home() {
   return (
