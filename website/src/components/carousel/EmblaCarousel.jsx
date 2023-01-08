5673d478ca99385df294b7735216551c40a65b73
Mani Batra <manibatra2002@gmail.com>
2023-01-08 16:06:30 +1100

    Add thumbnails to the carousel

--- website/src/components/carousel/EmblaCarousel.jsx
+++ website/src/components/carousel/EmblaCarousel.jsx
@@ -1,81 +1,100 @@
 import React, { useState, useEffect, useCallback } from 'react'
 import useEmblaCarousel from 'embla-carousel-react'
 import {
-    DotButton,
-    PrevButton,
-    NextButton,
+  DotButton,
+  PrevButton,
+  NextButton,
 } from './EmblaCarouselArrowsDotsButtons'
+import { Thumb } from '@/components/carousel/EmblaCarouselThumbsButton'
 
-const EmblaCarousel = ({slides, options}) => {
-    const [emblaRef, emblaApi] = useEmblaCarousel(options)
-    const [prevBtnEnabled, setPrevBtnEnabled] = useState(false)
-    const [nextBtnEnabled, setNextBtnEnabled] = useState(false)
-    const [selectedIndex, setSelectedIndex] = useState(0)
-    const [scrollSnaps, setScrollSnaps] = useState([])
+const EmblaCarousel = ({ slides, options }) => {
+  const [emblaRef, emblaApi] = useEmblaCarousel(options)
+  const [emblaThumbsRef, emblaThumbsApi] = useEmblaCarousel({
+    containScroll: 'keepSnaps',
+    dragFree: true,
+  })
+  const [prevBtnEnabled, setPrevBtnEnabled] = useState(false)
+  const [nextBtnEnabled, setNextBtnEnabled] = useState(false)
+  const [selectedIndex, setSelectedIndex] = useState(0)
+  const [scrollSnaps, setScrollSnaps] = useState([])
 
-    const scrollPrev = useCallback(
-        () => emblaApi && emblaApi.scrollPrev(),
-        [emblaApi],
-    )
-    const scrollNext = useCallback(
-        () => emblaApi && emblaApi.scrollNext(),
-        [emblaApi],
-    )
-    const scrollTo = useCallback(
-        (index) => emblaApi && emblaApi.scrollTo(index),
-        [emblaApi],
-    )
+  const scrollPrev = useCallback(
+    () => emblaApi && emblaApi.scrollPrev(),
+    [emblaApi]
+  )
+  const scrollNext = useCallback(
+    () => emblaApi && emblaApi.scrollNext(),
+    [emblaApi]
+  )
 
-    const onSelect = useCallback(() => {
-        if (!emblaApi) return
-        setSelectedIndex(emblaApi.selectedScrollSnap())
-        setPrevBtnEnabled(emblaApi.canScrollPrev())
-        setNextBtnEnabled(emblaApi.canScrollNext())
-    }, [emblaApi, setSelectedIndex])
+  const onThumbClick = useCallback(
+    (index) => {
+      if (!emblaApi || !emblaThumbsApi) return
+      if (emblaThumbsApi.clickAllowed()) emblaApi.scrollTo(index)
+    },
+    [emblaApi, emblaThumbsApi]
+  )
 
-    useEffect(() => {
-        if (!emblaApi) return
-        onSelect()
-        setScrollSnaps(emblaApi.scrollSnapList())
-        emblaApi.on('select', onSelect)
-        emblaApi.on('reInit', onSelect)
-    }, [emblaApi, setScrollSnaps, onSelect])
+  const onSelect = useCallback(() => {
+    if (!emblaApi || !emblaThumbsApi) return
+    setSelectedIndex(emblaApi.selectedScrollSnap())
+    setPrevBtnEnabled(emblaApi.canScrollPrev())
+    setNextBtnEnabled(emblaApi.canScrollNext())
+    emblaThumbsApi.scrollTo(emblaApi.selectedScrollSnap())
+  }, [emblaApi, emblaThumbsApi, setSelectedIndex])
 
-    return (
-        <>
-            <div className="embla">
-                <div className="embla__viewport" ref={emblaRef}>
-                    <div className="embla__container">
-                        {slides.map((imagePath, index) => (
-                            <div className="embla__slide" key={index}>
-                                <div className="embla__slide__number">
-                                    <span>{index + 1}</span>
-                                </div>
-                                <img
-                                    className="embla__slide__img"
-                                    src={imagePath}
-                                    alt="Your alt text"
-                                />
-                            </div>
-                        ))}
-                    </div>
+  useEffect(() => {
+    if (!emblaApi) return
+    onSelect()
+    setScrollSnaps(emblaApi.scrollSnapList())
+    emblaApi.on('select', onSelect)
+    emblaApi.on('reInit', onSelect)
+  }, [emblaApi, setScrollSnaps, onSelect])
+
+  return (
+    <>
+      <div className="embla">
+        <div
+          className="embla__viewport rounded-2xl border-2 border-solid border-gray-200"
+          ref={emblaRef}
+        >
+          <div className="embla__container">
+            {slides.map((imagePath, index) => (
+              <div className="embla__slide" key={index}>
+                <div className="embla__slide__number">
+                  <span>{index + 1}</span>
                 </div>
+                <img
+                  className="embla__slide__img"
+                  src={imagePath}
+                  alt="Your alt text"
+                />
+              </div>
+            ))}
+          </div>
+        </div>
 
-                <PrevButton onClick={scrollPrev} enabled={prevBtnEnabled} />
-                <NextButton onClick={scrollNext} enabled={nextBtnEnabled} />
-            </div>
+        <PrevButton onClick={scrollPrev} enabled={prevBtnEnabled} />
+        <NextButton onClick={scrollNext} enabled={nextBtnEnabled} />
+      </div>
 
-            <div className="embla__dots">
-                {scrollSnaps.map((_, index) => (
-                    <DotButton
-                        key={index}
-                        selected={index === selectedIndex}
-                        onClick={() => scrollTo(index)}
-                    />
-                ))}
-            </div>
-        </>
-    )
+      <div className="embla-thumbs">
+        <div className="embla-thumbs__viewport" ref={emblaThumbsRef}>
+          <div className="embla-thumbs__container">
+            {slides.map((imagePath, index) => (
+              <Thumb
+                onClick={() => onThumbClick(index)}
+                selected={index === selectedIndex}
+                index={index}
+                imgSrc={imagePath}
+                key={index}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default EmblaCarousel
--- website/src/components/carousel/EmblaCarouselThumbsButton.jsx
+++ website/src/components/carousel/EmblaCarouselThumbsButton.jsx
@@ -1,28 +1,28 @@
 import React from 'react'
 
 export const Thumb = (props) => {
-    const { selected, imgSrc, index, onClick } = props
+  const { selected, imgSrc, index, onClick } = props
 
-    return (
-        <div
-            className={'embla-thumbs__slide'.concat(
-                selected ? ' embla-thumbs__slide--selected' : '',
-            )}
-        >
-            <button
-                onClick={onClick}
-                className="embla-thumbs__slide__button"
-                type="button"
-            >
-                <div className="embla-thumbs__slide__number">
-                    <span>{index + 1}</span>
-                </div>
-                <img
-                    className="embla-thumbs__slide__img"
-                    src={imgSrc}
-                    alt="Your alt text"
-                />
-            </button>
+  return (
+    <div
+      className={'embla-thumbs__slide '.concat(
+        selected ? ' embla-thumbs__slide--selected' : ''
+      )}
+    >
+      <button
+        onClick={onClick}
+        className="embla-thumbs__slide__button"
+        type="button"
+      >
+        <div className="embla-thumbs__slide__number invisible md:visible">
+          <span>{index + 1}</span>
         </div>
-    )
+        <img
+          className="embla-thumbs__slide__img rounded-2xl border-[1px] border-solid border-gray-200"
+          src={imgSrc}
+          alt="Your alt text"
+        />
+      </button>
+    </div>
+  )
 }
